b86f24e7092157f50604f3d8358c2196b92f235d
michael oudenhuijsen <michael.oudenhuijsen@pangaea.nl>
2020-01-29 00:02:45 +0100

    V1.1

--- server.js
+++ server.js
@@ -15,16 +15,25 @@ app.use(cors())
 //GET JSONS
 var releaseFileExists = fsFunctions.fileExists('./data/releases.json');
 if(!releaseFileExists){
-    initFunctions.initAll()
+    initFunctions.initRelease()
+}
+var errorLoggingDirectoryExists = fsFunctions.mapExists('./data/errorlogging');
+if(!errorLoggingDirectoryExists){
+    initFunctions.initErrorDirectory();
 }
 //Get all json info
 var releases = fsFunctions.readFileSync('./data/releases.json');
 
 //CREATE SERVER
 
-var server = app.listen( process.env.PORT);
+var server = app.listen(3001);
 function listening() {
     console.log('server listening');
+}
+
+function finished
+ (err) {
+    console.log('write complete', err)
 }
 
 //ADD WEBSITE INDEX
@@ -36,7 +45,44 @@ app.get('/api/releases', getReleases);
 function getReleases(request, response) {
     response.send(releases);
 }
+//POST ERROR
+app.post('/api/errorlogging', function (request, res) {
+    let errorLogs = ''
+    const {url} = request.body;
+    const params = request.body;
+    
+    const splittedUrl = url.split('/static');
+    params.realUrl = splittedUrl[0];
 
+    //var releaseFileExists = fsFunctions.fileExists('./data/releases.json');
+    const currentTime = new Date();
+
+    // returns the month (from 0 to 11)
+    const month = currentTime.getMonth() + 1;
+
+    // returns the day of the month (from 1 to 31)
+    const day = currentTime.getDate();
+
+    // returns the year (four digits)
+    const year = currentTime.getFullYear();
+    const date = day + "-" + month + "-" + year;
+
+    const errorFileExists = fsFunctions.fileExists(`./data/errorlogging/${date}.json`);
+    if(!errorFileExists){
+        const newArray = [params]
+        initFunctions.initErrorFile(date, newArray)
+    }else{
+        console.log(2);
+        errorLogs = fsFunctions.readFileSync(`./data/errorlogging/${date}.json`);
+        errorLogs.unshift(params)
+
+        fs.writeFile(`./data/errorlogging/${date}.json`, JSON.stringify(errorLogs, null, 2), finished)
+    
+        response.send(errorLogs)
+    }
+
+    res.send(errorLogs);
+})
 app.get('*', (req,res) =>{
     res.sendFile(path.join(__dirname+'/mothership-app/build/index.html'));
 });

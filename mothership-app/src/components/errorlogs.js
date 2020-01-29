584bd1ebd2d94439fe51f925c030fe5ef6a14b2d
michael oudenhuijsen <michael.oudenhuijsen@pangaea.nl>
2020-01-29 22:57:20 +0100

    v.1.3 errors aangepast naar selector

--- mothership-app/src/components/errorlogs.js
+++ mothership-app/src/components/errorlogs.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
-import { StyledBody } from '../styled-components' 
+import { Link } from "react-router-dom";
+import { StyledBody, StyledLogTD } from '../styled-components' 
 
 function Errorlogs(props) {
-    const releaseList = props.errors.map((error) => {
-    return <tr><td>{error.msg}</td><td>{error.realUrl}</td><td>{error.dateCreated}</td></tr>
+    
+    const errorList = props.errorlogs.map((error) => {
+    const splitLogs = error.split('.json');
+    const logname = splitLogs[0];
+    return <tr key={logname}><StyledLogTD><Link to={`/errorlogs/${logname}`}>{logname}</Link></StyledLogTD></tr>
     });
     const codeBlock = 'window.onerror = async function(msg, url, line) {' +
                         'await fetch("https://mothership.pangaeatest.nl/api/errorlogging", {' +
@@ -19,6 +23,14 @@ function Errorlogs(props) {
             <h1>Errorlogs</h1>
             <p>Een overzicht van de errors in alle projecten die CMS 6.3.0 of hoger hebben.</p>
             
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Log dates</th>
+                    </tr>
+                    {errorList}
+                </tbody>
+            </table>
             <p> Get request: </p>
             <p> /api/errorlogging </p>
             <p>Om een project te kunnen loggen moet er ergens in je javascript dit worden toegevoegd</p>
@@ -26,17 +38,6 @@ function Errorlogs(props) {
             <p style={{padding: '20px 0px'}}>
                     {codeBlock}
             </p>
-            <table>
-                <tbody>
-                    <tr>
-                        <th>Error message</th>
-                        <th>url</th>
-                        <th>datetime</th>
-                    </tr>
-                    {releaseList}
-                </tbody>
-            </table>
-            
         </StyledBody>
     </React.Fragment>
   );
---
+++ mothership-app/src/components/errorlogsdetail.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import { withRouter } from "react-router-dom";
+
+import { StyledBody } from '../styled-components' 
+
+//constants
+import { API_ROOT } from "../constants/endpoints";
+
+function ErrorlogsDetail(props) {
+    console.log(props);
+  const [errors, setErrors] = useState([]);
+  
+  useEffect(() => {
+    async function fetchErrors() {
+      const response = await fetch(`${API_ROOT}errorlogging/${props.match.params.date}`);
+      const json = await response.json();
+      
+      setErrors(json);
+    }
+
+    fetchErrors();
+    
+  }, errors);
+
+
+    const errorList = errors.map((error, index) => {
+    return <tr key={index}><td>{error.msg}</td><td>{error.realUrl}</td><td>{error.dateCreated}</td></tr>
+    });
+
+    return (
+    <React.Fragment>
+        <StyledBody>
+            <h1>Errorlogs</h1>
+            <p>Een overzicht van de errors in alle projecten die CMS 6.3.0 of hoger hebben.</p>
+            
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Error message</th>
+                        <th>url</th>
+                        <th>datetime</th>
+                    </tr>
+                    {errorList}
+                </tbody>
+            </table>
+            <p> Get request: </p>
+            <p> /api/errorlogging/:date </p>
+            <p> Param date: day-month-year / 20-1-2020 </p>
+            
+        </StyledBody>
+    </React.Fragment>
+  );
+}
+
+export default withRouter(ErrorlogsDetail);
